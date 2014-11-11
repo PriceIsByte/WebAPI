848d54d6956d3621dc14461cb829d837e484bbc4
Jon Price <jon419@gmail.com>
2014-11-11 00:31:50 -0500

    add json caml case contract resolver and new food route

--- CountingKs/Controllers/FoodsController.cs
+++ CountingKs/Controllers/FoodsController.cs
@@ -1,11 +1,7 @@
 ﻿using CountingKs.Data;
-using CountingKs.Data.Entities;
 using CountingKs.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace CountingKs.Controllers
--- CountingKs/Models/ModelFactory.cs
+++ CountingKs/Models/ModelFactory.cs
@@ -1,9 +1,6 @@
 ﻿using CountingKs.Data.Entities;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CountingKs.Models
 {
