2f522dd90350040582c65eebcb09ccf8e77df406
Jon Price <jon419@gmail.com>
2014-11-10 23:39:03 -0500

    Add foods controller

---
+++ CountingKs/Controllers/FoodsController.cs
@@ -0,0 +1,29 @@
+﻿using CountingKs.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CountingKs.Controllers
+{
+    public class FoodsController : ApiController
+    {
+        private readonly ICountingKsRepository _repo;
+
+        public FoodsController(ICountingKsRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public object Get()
+        {
+            var result = _repo.GetAllFoods()
+                                .OrderBy(f => f.Description)
+                                .Take(25)
+                                .ToList();
+            return result;
+        }
+    }
+}
