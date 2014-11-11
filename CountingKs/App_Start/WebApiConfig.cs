8f94e1a802b6f7a5fc5fa8652126ad1b68744760
Jon Price <jon419@gmail.com>
2014-11-11 12:40:10 -0500

    Add measures controller and route Start adding url to food and measure model

--- CountingKs/App_Start/WebApiConfig.cs
+++ CountingKs/App_Start/WebApiConfig.cs
@@ -11,8 +11,14 @@ namespace CountingKs
     {
         config.Routes.MapHttpRoute(
             name: "Food",
-            routeTemplate: "api/nutrition/foods/{id}",
-            defaults: new { controller = "Foods", id = RouteParameter.Optional }
+            routeTemplate: "api/nutrition/foods/{foodid}",
+            defaults: new { controller = "Foods", foodid = RouteParameter.Optional }
+        );
+
+        config.Routes.MapHttpRoute(
+            name: "Measures",
+            routeTemplate: "api/nutrition/foods/{foodid}/measures/{id}",
+            defaults: new { controller = "Measures", id = RouteParameter.Optional }
         );
 
         // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
---
+++ CountingKs/Controllers/MeasuresController.cs
@@ -0,0 +1,42 @@
+﻿using CountingKs.Data;
+using CountingKs.Data.Entities;
+using CountingKs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CountingKs.Controllers
+{
+    public class MeasuresController : ApiController
+    {
+        private readonly ICountingKsRepository _repo;
+        private readonly ModelFactory _modelFactory;
+
+        public MeasuresController(ICountingKsRepository repo)
+        {
+            _repo = repo;
+            _modelFactory = new ModelFactory();
+        }
+
+        public IEnumerable<MeasureModel> Get(int foodid)
+        {
+            var results = _repo.GetMeasuresForFood(foodid)
+                               .ToList()
+                               .Select(m => _modelFactory.Create(m));
+
+            return results;
+        }
+
+        public MeasureModel Get(int foodid, int id)
+        {
+            var result = _repo.GetMeasure(id);
+            if (result == null)
+                return null;
+
+            return result.Food.Id == foodid ? _modelFactory.Create(result) : null;
+        }
+    }
+}
