9d52064154258ec9e37d2dcad8805cc72835178d
Jon Price <jon419@gmail.com>
2014-11-10 23:48:30 -0500

    add kernel binding to ninject

--- CountingKs/App_Start/NinjectWebCommon.cs
+++ CountingKs/App_Start/NinjectWebCommon.cs
@@ -10,6 +10,7 @@ namespace CountingKs.App_Start
 
     using Ninject;
     using Ninject.Web.Common;
+    using CountingKs.Data;
 
     public static class NinjectWebCommon 
     {
@@ -61,6 +62,8 @@ namespace CountingKs.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            kernel.Bind<ICountingKsRepository>().To<CountingKsRepository>();
+            kernel.Bind<CountingKsContext>().To<CountingKsContext>();
         }        
     }
 }
